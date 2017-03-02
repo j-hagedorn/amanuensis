70d0db92e6bbfab6e8219b36eb75e47abd293124
Joshh <josh.hagedorn@gmail.com>
2017-03-01 23:31:40 -0500

    Try tweaks

--- found_sound.R
+++ found_sound.R
@@ -1,21 +1,27 @@
 
-found_sound <- function(letters = "aa", word_list = words, place = "anywhere") {
+found_sound <- function(word_list = words, letters = "aa", place = "anywhere") {
   
-  # word_list must be a data.frame, fun will take the first column
+  library(dplyr); library(magrittr)
   # function returns a data.frame with the subset of a vector of words 
   # which match the defined pattern
   
+  # word_list must be a data.frame, fun will take the column named "word"
+  # if no column is titled "word" it takes the first column
+  if ("word" %in% names(word_list)) {
+    x <- word_list %>% select(word)
+  } else x <- word_list[,1]
+  
   if (place == "anywhere") {
     
-    word <- grep(letters, word_list[,1], value = T, ignore.case = T)
+    word <- grep(letters, x, value = T, ignore.case = T)
     
   } else if (place == "start") {
     
-    word <- grep(paste0("^",letters), word_list[,1], value = T, ignore.case = T)
+    word <- grep(paste0("^",letters), word_list, value = T, ignore.case = T)
     
   } else if (place == "end") {
     
-    word <- grep(paste0(letters,"$"), word_list[,1], value = T, ignore.case = T)
+    word <- grep(paste0(letters,"$"), word_list, value = T, ignore.case = T)
     
   } else print("Not a defined location")
   
