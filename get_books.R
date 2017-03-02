ac8997de33aa3e9642d599a311a33f1579128b34
Joshh <josh.hagedorn@gmail.com>
2017-03-01 23:38:04 -0500

    Add books

--- get_books.R
+++ get_books.R
@@ -13,34 +13,101 @@ rm(words1);rm(words2);rm(words3)
 words %<>% distinct()
 
 # Get works from Project Gutenberg
-library(gutenbergr)
+library(gutenbergr); library(tidytext); library(stringr)
 
-gutenberg_works() %>% filter(str_detect(author, 'Dante'))
+gutenberg_works() %>% 
+  filter(str_detect(author, 'Wells')) %>%
+  View()
 
-tst <-
-gutenberg_download(1004) %>%
+##### Paradise Lost ######
+
+paradiselost <-
+gutenberg_download(20) %>%
+  # Label books
+  mutate(book = cumsum(str_detect(text,regex("^  BOOK",ignore_case = F)))) %>%
+  # Remove filler text between books
+  filter(text != "  PARADISE LOST") %>%
+  filter(grepl("*THE END OF THE*",text) == F) %>%
   # Remove header and footer matter
-  slice(269:19842) %>%
+  slice(26:10980) %>%
   # Number lines for reference
   mutate(line = row_number()) %>%
   unnest_tokens(word, text) %>%
   mutate(word = str_extract(word, "[a-z]+"))
 
+##### Leaves of Grass ######
+
+leaves <-
+  gutenberg_download(1322) %>%
+  # Label books
+  mutate(book = cumsum(str_detect(text,regex("^BOOK ",ignore_case = F)))) %>%
+  # Remove header and footer matter
+  slice(23:17859) %>%
+  # Number lines for reference
+  mutate(line = row_number()) %>%
+  unnest_tokens(word, text) %>%
+  mutate(word = str_extract(word, "[a-z]+"))
+
+##### Leaves of Grass ######
+
+leaves <-
+  gutenberg_download(1322) %>%
+  # Label books
+  mutate(book = cumsum(str_detect(text,regex("^BOOK ",ignore_case = F)))) %>%
+  # Remove header and footer matter
+  slice(23:17859) %>%
+  # Number lines for reference
+  mutate(line = row_number()) %>%
+  unnest_tokens(word, text) %>%
+  mutate(word = str_extract(word, "[a-z]+"))
+
+##### Moby Dick ######
+
+moby <-
+  gutenberg_download(2489) %>%
+  # Label chapters
+  mutate(chapter = cumsum(str_detect(text,regex("^*CHAPTER [[:digit:]]",ignore_case = F)))) %>%
+  # Number lines for reference
+  mutate(line = row_number()) %>%
+  unnest_tokens(word, text) %>%
+  mutate(word = str_extract(word, "[a-z]+"))
+
+##### Keats' Collected Works #####
+
+keats <-
+  gutenberg_download(c(8209,23684)) %>%
+  # Remove header and footer matter
+  slice(21:6253)
+
+#### Frost Poems
+
+frost <- gutenberg_download(c(3021, 3026, 29345))
+
+##### Donne Poems
+
+donne <- gutenberg_download(c(48688, 48772))
+
+##### Pound
+
+pound <- gutenberg_download(c(23538,40200,41162,51992))
+
+##### Emily Dickinson
+
+emily <- 
+  gutenberg_download(12242) %>%
+  # Remove header and footer matter
+  slice(157:10304) %>%
+  unnest_tokens(word, text) 
+
+
 # Get a library...
 
 # shakespeare: 100
-# paradiselost: 20
-# leavesofgrass: 1322
-# mobydick: 2701
 # grimm: 2591
 # metamorphosis: 5200
 # jekyll: 43
 # pope_iliad: 6130
 # pound_personae: 41162
-# frost_boyswill: 3021
-# frost_mtintrvl: 29345
-# frost_noboston: 3026
-# keats: 23684
 # emily: 12242
 # ulysses: 4300
 
