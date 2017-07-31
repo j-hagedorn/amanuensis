4edbad17cd23f4bbd7fb8ae94f045162b8a4e0ff
Joshh <josh.hagedorn@gmail.com>
2017-07-30 21:47:53 -0400

    Define functions for sourcing

--- tools/simil.R
+++ tools/simil.R
@@ -1,60 +1,50 @@
 
-library(stringdist); library(tidyr); library(dplyr)
+# Load dependencies
 
-tot_words <- nrow(commedia)
+library(stringdist); library(tidyverse)
 
-str1 <- 
-  commedia %>% 
-  filter(is.na(word) == F) %>%
-  count(word, sort = T) %>%  
-  mutate(total = tot_words,
-         rank = row_number(),
-         freq = n/total) %>%
-  slice(1:500)
+# Define functions
+
+rearrange <- function(string1,string2,alg = "jw",goldilocks=c(0.01,0.30)) {
+  # Function takes 2 vectors of strings, of the same size
+  # and returns a dataframe of matches based on similarity
+  # Args:
+    # string1,string2 : the 2 string vectors
+    # alg: Algorithm used for similarity, passed to stringdist::stringdistmatrix()
+    # goldilocks: Numeric vector of the low and high scores for matching accuracy
   
-str2 <- 
-  leaves %>% 
-  filter(is.na(word) == F) %>%
-  count(word, sort = T) %>%  
-  mutate(total = tot_words,
-         rank = row_number(),
-         freq = n/total) %>%
-  slice(1:500)
-
-str1 <- pound_trigrams %>% select(word = trigram) %>% sample_n(1000) %>% distinct() 
-str2 <- frost_trigrams %>% select(word = trigram) %>% sample_n(1000) %>% distinct()  
-
-
-str1 <- words %>% anti_join(stop_words) %>% sample_n(1000)
-str2 <- words %>% anti_join(stop_words) %>% sample_n(1000)
-
-
-dist <- stringdist(str1$word, str2$word, method = "soundex")
-
-rearrange <- 
   # Create distance matrix computing string diff
-  stringdistmatrix(str1$word, str2$word, method='jw', useNames = T)  %>%
-  as.data.frame() %>%
-  mutate(rawtext = row.names(.)) %>%
-  group_by(rawtext) %>%
-  gather(coded,score,-rawtext) %>%
-  mutate(rank = row_number(score)) %>%
-  # keep only the most accurate
-  filter(score >= 0.01, score < 0.30)
+  stringdistmatrix(string1,string2, method = alg, useNames = T)  %>%
+    as.data.frame() %>%
+    mutate(rawtext = row.names(.)) %>%
+    group_by(rawtext) %>%
+    gather(coded,score,-rawtext) %>%
+    mutate(rank = row_number(score)) %>%
+    # keep only the most accurate
+    filter(score >= goldilocks[1], score < goldilocks[2])
+
+}
   
-rhyme <- 
-  # Create distance matrix computing string diff
-  stringdistmatrix(str1$word, str2$word, method='soundex', useNames = T)  %>%
-  as.data.frame() %>%
-  mutate(rawtext = row.names(.)) %>%
-  group_by(rawtext) %>%
-  gather(coded,score,-rawtext) %>%
-  mutate(rank = row_number(score)) %>%
-  # keep only rhymes
-  filter(score == 0) %>%
-  # remove exact rhymes
-  filter(rawtext != coded)
+rhyme <- function(string1,string2) {
+  # Function takes 2 vectors of strings, of the same size
+  # and returns a dataframe of matches based on similarity
+  # Args:
+  # string1,string2 : the 2 string vectors
 
+  # Create distance matrix computing string diff
+  stringdistmatrix(string1,string2, method = 'soundex', useNames = T)  %>%
+    as.data.frame() %>%
+    mutate(rawtext = row.names(.)) %>%
+    group_by(rawtext) %>%
+    gather(coded,score,-rawtext) %>%
+    mutate(rank = row_number(score)) %>%
+    # keep only rhymes
+    filter(score == 0) %>%
+    # remove exact rhymes
+    filter(rawtext != coded)
+  
+}
+  
 # Notes:
 # Option to remove suffixes (e.g. -ness, -est, etc.)
 # Option to remove words with same first letter
@@ -62,10 +52,3 @@ rhyme <-
 # Pass df of words in chunks to stringdistmatrix() and combine (so don't have to slice)
 # Remove dups from strings before passing to stringdistmatrix()
 
-  tst2 <-
-  tst %>%
-  # select match with lowest (best) score
-  # filter(rank == min(rank)) %>%
-  # keep only the most accurate
-  filter(score >= 0.01, score < 0.30) %>%
-  select(rawtext,coded)
