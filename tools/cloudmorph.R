278a5423e37045db97ebe27799f3cf861e9add39
Joshh <josh.hagedorn@gmail.com>
2017-10-06 11:46:38 -0400

    Init clouds

--- tools/cloudmorph.R
+++ tools/cloudmorph.R
@@ -7,6 +7,9 @@ library(stringdist); library(tidyverse); library(tidytext)
 str1 <- words %>% anti_join(stop_words) %>% sample_n(5000)
 str2 <- words %>% anti_join(stop_words) %>% sample_n(5000)
 
+str1[[1]]
+
+
 morphose <- 
   # Create distance matrix computing string diff
   stringdistmatrix(str1$word, str2$word, method='hamming', useNames = T)  %>%
@@ -24,8 +27,29 @@ tst <- morphose %>% kmeans(50)
 tst <- tst %>% augment(morphose) %>% select(.cluster, everything())
 
 # Start with word (either passed as arg or randomly selected)
+
+x <- data.frame()
+
+for(i in words$word[111:250]) {
+  dist <- stringdist(i,words$word)
+  df <- words
+  df$dist <- dist
+  
+  df %<>% 
+    mutate(
+      word1 = i,
+      word2 = word
+    ) %>%
+    filter(dist == 1) %>%
+    select(word1,word2)
+  
+  x %<>% bind_rows(df)
+}
+
+
 # Find all words that can be created by replacing one letter in word
-# Find all words that can be created by switching two letters in word
+
+
 
 # Calculated 'hamming' stringdist
 stringdistmatrix("leviathan",words, method='hamming')
