10eda4e937f589c11c7a87a00b06f8a48810d252
Joshh <josh.hagedorn@gmail.com>
2017-12-10 16:04:07 -0500

    Add prepositions to words

--- tools/get_words.R
+++ tools/get_words.R
@@ -11,3 +11,15 @@ words <- rbind(words1,words2,words3,word_corpus)
 rm(words1);rm(words2);rm(words3);rm(word_corpus)
 
 words %<>% distinct()
+
+# Find all prepositional phrases (join to wordlist)
+
+prepositions <- 
+  read_csv(
+    "http://scrapmaker.com/data/wordlists/language/prepositions.txt", 
+    col_names = F
+  ) %>%
+  rename(word = X1) %>%
+  mutate(word = str_trim(word))
+
+# Find all words with e.g. only 1 vowel
