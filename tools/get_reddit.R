683a568af335fc479447168dadea90f30666941f
Joshh <j-hagedorn@users.noreply.github.com>
2020-10-23 12:54:39 -0400

    Update get_reddit.R

--- tools/get_reddit.R
+++ tools/get_reddit.R
@@ -1,28 +1,28 @@
-get_r_reddit("dataisbeautiful", n = 100000)
 
-url <- "https://www.reddit.com/r/AskReddit/comments/j0z4lp/formerly_suicidal_redditors_whats_something_that/"
+# "https://www.reddit.com/r/AskReddit/comments/j0z4lp/formerly_suicidal_redditors_whats_something_that/"
 
-comments <- "https://api.pushshift.io/reddit/submission/comment_ids/j0z4lp"
+link_id <- "j0z4lp"
 
 library(tidyverse); library(jsonlite)
-reddit <- fromJSON("https://www.reddit.com/r/AskReddit/comments/j0z4lp/formerly_suicidal_redditors_whats_something_that/.json")
+
+# Get list of comment ids from Pushshift API
+comment_id_list <- fromJSON(paste0("https://api.pushshift.io/reddit/submission/comment_ids/",link_id))
+
+# Split into chunks to limit API query rate (assumes 200/minute)
+chunks <- split(comment_id_list$data, ceiling(seq_along(comment_id_list$data)/200))
+
+df <- tibble()
+
+for (i in chunks[1:3]){
+  
+  x <- fromJSON(paste0("https://api.pushshift.io/reddit/comment/search?ids=",paste(i,collapse = ",")))
+  x <- x$data %>% select(id,parent_id,link_id,subreddit,author_fullname,author,created_utc,body,score)
+  df <- bind_rows(df,x)
+  Sys.sleep(60)
+  
+}
 
 
-comment_id_list <- fromJSON(comments)
-
-comment_id_list$data[1:5]
-
-comments_id_url <- paste0("https://api.pushshift.io/reddit/comment/search?ids=",paste(comment_id_list$data[1:5],collapse = ","))
-
-comments_df <- fromJSON(comments_id_url)
-
-tst <- comments_df$data %>% as_tibble()
-
-tst <- reddit$data$children[[2]]$data
-tst <- reddit$data$children
-
-library(RedditExtractoR)
-x <- reddit_content(URL=url)
 
 
 # "https://www.reddit.com/r/redditdev/comments/agw2si/retrieve_all_comments_from_a_subreddit/"
