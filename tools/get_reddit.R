3ed073f82bd18371ba9058d48cafb33830633fe5
Joshh <j-hagedorn@users.noreply.github.com>
2020-10-23 13:03:52 -0400

    Update get_reddit.R

--- tools/get_reddit.R
+++ tools/get_reddit.R
@@ -1,9 +1,33 @@
 
 # "https://www.reddit.com/r/AskReddit/comments/j0z4lp/formerly_suicidal_redditors_whats_something_that/"
+library(tidyverse); library(jsonlite)
 
 link_id <- "j0z4lp"
 
-library(tidyverse); library(jsonlite)
+get_reddit <- function(link_id,rate_limit = 200){
+  
+  # Get list of comment ids from Pushshift API
+  comment_id_list <- fromJSON(paste0("https://api.pushshift.io/reddit/submission/comment_ids/",link_id))
+  
+  # Split into chunks to limit API query rate (assumes 200/minute)
+  chunks <- split(comment_id_list$data, ceiling(seq_along(comment_id_list$data)/rate_limit))
+  
+  df <- tibble()
+  
+  for (i in chunks){
+    
+    x <- fromJSON(paste0("https://api.pushshift.io/reddit/comment/search?ids=",paste(i,collapse = ",")))
+    x <- x$data %>% select(id,parent_id,link_id,subreddit,author_fullname,author,created_utc,body,score)
+    df <- bind_rows(df,x)
+    Sys.sleep(60)
+    
+  }
+  
+  return(df)
+  
+}
+
+
 
 # Get list of comment ids from Pushshift API
 comment_id_list <- fromJSON(paste0("https://api.pushshift.io/reddit/submission/comment_ids/",link_id))
@@ -13,7 +37,7 @@ chunks <- split(comment_id_list$data, ceiling(seq_along(comment_id_list$data)/20
 
 df <- tibble()
 
-for (i in chunks[1:3]){
+for (i in chunks){
   
   x <- fromJSON(paste0("https://api.pushshift.io/reddit/comment/search?ids=",paste(i,collapse = ",")))
   x <- x$data %>% select(id,parent_id,link_id,subreddit,author_fullname,author,created_utc,body,score)
