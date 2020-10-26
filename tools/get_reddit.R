3b28ed21979f51125d73cb8668ed0c65c21b4014
Joshh <j-hagedorn@users.noreply.github.com>
2020-10-26 11:01:22 -0400

    Update get_reddit.R

--- tools/get_reddit.R
+++ tools/get_reddit.R
@@ -1,6 +1,12 @@
 
 # "https://www.reddit.com/r/AskReddit/comments/j0z4lp/formerly_suicidal_redditors_whats_something_that/"
+# "https://www.reddit.com/r/AskReddit/comments/jgf6om/serious_whats_something_someone_told_you_when_you/"
+# "https://www.reddit.com/r/AskReddit/comments/ij7e5n/suicidal_people_of_reddit_what_helped_keep_you/"
+# "https://www.reddit.com/r/AskReddit/search?q=suicide&restrict_sr=1"
+
 library(tidyverse); library(jsonlite)
+
+# found_urls <- RedditExtractoR::reddit_urls(search_terms="suicide",subreddit = "AskReddit",cn_threshold = 50)
 
 link_id <- "j0z4lp"
 
@@ -15,12 +21,10 @@ get_reddit <- function(link_id,rate_limit = 200){
   df <- tibble()
   
   for (i in chunks){
-    
     x <- fromJSON(paste0("https://api.pushshift.io/reddit/comment/search?ids=",paste(i,collapse = ",")))
     x <- x$data %>% select(id,parent_id,link_id,subreddit,author_fullname,author,created_utc,body,score)
     df <- bind_rows(df,x)
     Sys.sleep(60)
-    
   }
   
   return(df)
