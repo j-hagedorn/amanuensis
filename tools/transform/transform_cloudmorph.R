25d22cfc166402c9ee02f851323b3bad24016246
Joshh <j-hagedorn@users.noreply.github.com>
2021-01-21 06:05:36 -0500

    Update transform_cloudmorph.R

--- tools/transform/transform_cloudmorph.R
+++ tools/transform/transform_cloudmorph.R
@@ -6,36 +6,45 @@ lexicon <- feather::read_feather("texts/output/lexicon.feather")
 
 seed_word = "erode"
 
-
-df <-
-  as_tibble(seed_word) %>%
-  rename(word = value) %>%
-  stringdist_left_join(
-    lexicon %>% mutate(word = as.character(word)) %>% select(word), 
-    by = "word", 
-    method = "osa",
-    max_dist = 1
-  ) %>%
-  rename(word_0 = word.x, word = word.y)
-
-# i = 1
-
-for (i in 1:2) {
+morph <- function(seed_word, n_changes = 5, dist_method = "osa", max_dist = 1){
   
-  x <-
-    df %>%
+  df <-
+    as_tibble(seed_word) %>%
+    rename(word = value) %>%
     stringdist_left_join(
       lexicon %>% mutate(word = as.character(word)) %>% select(word), 
       by = "word", 
       method = "osa",
       max_dist = 1
-    ) 
+    ) %>%
+    filter(word.x != word.y) %>%
+    rename(word_0 = word.x, word = word.y)
   
-  # Rename cols to allow next join
-  names(x) <- c("word_0",glue::glue("word_{n1}", n1 = rep(1:i)),"word")
+  for (i in 1:(n_changes - 2)) {
+    
+    x <-
+      df %>%
+      stringdist_left_join(
+        lexicon %>% mutate(word = as.character(word)) %>% select(word), 
+        by = "word", 
+        method = "osa",
+        max_dist = 1
+      ) %>%
+      filter(word.x != word.y) %>%
+      filter(word.y != seed_word)
+    
+    # Rename cols to allow next join
+    names(x) <- c("word_0",glue::glue("word_{n1}", n1 = rep(1:i)),"word")
+    
+    df <- x
+    
+  }
   
-  df <- x
+  return(df)
   
 }
+
+
+
 
 
